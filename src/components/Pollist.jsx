0ffcb0b2c11d77a633f2c0fbbdf8b8f6cbf592c5
Erika Gwiyo <erika.gwiyo@student.moringaschool.com>
2026-05-01 23:32:41 +0300

    addition of a way to disable votes and vote once

--- src/components/Pollist.jsx
+++ src/components/Pollist.jsx
@@ -1,12 +1,26 @@
-import pollopption from "./Polloption";
-import "./App.css";
-function Polllist({polls, onVote}) {
+import PollOption from "./PollOption";
+import "../App.css";
+function PollList({ polls, onVote, isOptionDisabled }) {
   return (
     <div>
-        {polls.map((poll) => (
-            <pollopption key={poll.id} poll={poll} onVote={onVote} />
-        ))}
+      {polls.map((poll) => {
+        const totalVotes = poll.options.reduce((sum, option) => sum + option.votes, 0);
+        return (
+          <section key={poll.id} className="poll-section">
+            <h2>{poll.text}</h2>
+            {poll.options.map((option) => (
+              <PollOption
+                key={option.id}
+                option={option}
+                vote={() => onVote(poll.id, option.id)}
+                totalVotes={totalVotes}
+                disabled={isOptionDisabled ? isOptionDisabled(poll.id, option.id) : false}
+              />
+            ))}
+          </section>
+        );
+      })}
     </div>
-    );
+  );
 }
-export default Polllist;
+export default PollList;
