1ee07b9296b812daa2877f70cfa246647fd12f17
Erika Gwiyo <erika.gwiyo@student.moringaschool.com>
2026-05-04 09:45:44 +0300

    ensuring vote is once

--- src/components/PollList.jsx
+++ src/components/PollList.jsx
@@ -1,18 +1,24 @@
-import PollOption from "./PollOption";
-
-
-function PollList({ polls, onVote, isOptionDisabled, }) {
+import PollOption from "./PollOption"
+import{ useState } from "react";
+function PollList({ polls, onVote }) {
   const totalVotes = polls.reduce((sum, poll) => sum + poll.votes, 0);
+  const [hasVoted, setHasVoted] = useState(false);
+  const handleVote = (pollId) => {
+    if (!hasVoted) {
+      onVote(pollId);
+      setHasVoted(true);
+    }
+  };
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Poll Options</h2>
       {polls.map((poll) => (
         <PollOption
           key={poll.id}
+          disabled={hasVoted}
           option={poll}
-          vote={() => onVote(poll.id)}
+          vote={() => handleVote(poll.id)}
           totalVotes={totalVotes}
-          disabled={isOptionDisabled ? isOptionDisabled(poll.id) : false}
         />
       ))}
     </div>
