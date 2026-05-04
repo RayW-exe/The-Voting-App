da10c626d780bd369a814071081a05b01fea875b
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-04 15:29:59 +0300

    deploy

--- src/components/PollOption.jsx
+++ src/components/PollOption.jsx
@@ -10,10 +10,12 @@ function PollOption({ option , totalVotes, vote, disabled}){
             <p>{option.votes} votes ({percentage}%)</p>
             <div className="w-full bg-gray-300 rounded h-4 mt-2">
          <div
-             className="bg-green-500 h-4 rounded p-2"
+             className="bg-green-500 h-4 rounded p-1 transition-all duration-500 ease-in-out"
    
              style={{ width: `${percentage}%` }}
   ></div>
+  <br />
+  <br />
 </div>
     
      <button onClick={vote} disabled={disabled} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
