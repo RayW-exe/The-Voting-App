911c80190a861af826bf0900fb70cefba8fa4bfa
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-05 08:58:43 +0300

    Deploy

--- src/components/PollOption.jsx
+++ src/components/PollOption.jsx
@@ -14,8 +14,6 @@ function PollOption({ option , totalVotes, vote, disabled}){
    
              style={{ width: `${percentage}%` }}
   ></div>
-  <br />
-  <br />
 </div>
     
      <button onClick={vote} disabled={disabled} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
-  base: "/voting-app/"
+  base: "/The-Voting-App"
 })
