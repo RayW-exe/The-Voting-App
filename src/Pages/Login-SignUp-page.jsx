bf755e98ae21630c9bf51016994c7ce2e9c520f2
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-06 08:01:43 +0300

    Fixed login page

--- src/Pages/Login-SignUp-page.jsx
+++ src/Pages/Login-SignUp-page.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { app } from "../firebase";
-import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,29 +9,44 @@ function LoginSignUpPage() {
     const navigate = useNavigate();
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
-    const [errors,setErrors] = useState({})
-    const [success,setSuccess] = useState("")
+    const [loginError,setLoginError] = useState("")
 
     const auth = getAuth(app);
     
-    const handleSignUp = async () => {
+    const handleSignUp = async (e) => {
         try {
             await createUserWithEmailAndPassword(auth, email, password);
-            setSuccess("User signed up successfully");
-            setErrors({});
+            console.log("SignUp Done");
             navigate("/voting")
-        } catch {
-            setErrors({ form: "Error signing up. Please try again." });
-            setSuccess("");
+        } catch (error) {
+            console.log(error)
         }
     }
 
-    const handleSubmit = (e) => {
+    const handleLogin = async(e) => {
         e.preventDefault()
-        setSuccess("Login submitted")
-        setErrors({})
-        navigate("/voting")
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            console.log("Login Successful");
+            navigate("/voting")
+            
+        } catch (error) {
+            console.log(error.code)
+        }
     }
+    const handleLoginWithGoogle = async(e) => {
+        e.preventDefault()
+        try {
+            const provider = new GoogleAuthProvider();
+            await signInWithPopup(auth, provider);
+            console.log("Login Successful");
+            navigate("/voting")
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
       <>
       <div className="bg-gray-200 min-h-screen p-4 flex flex-col items-center justify-center">
@@ -39,7 +54,7 @@ function LoginSignUpPage() {
                 <div className="text-3xl font-bold mb-4 align-middle text-center">
                     <h1>LOGIN / SIGN UP</h1>
                 </div>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleLogin}>
                         <div className="mb-4">
                             <input type="email" 
                             name="email" 
@@ -48,7 +63,7 @@ function LoginSignUpPage() {
                             onChange={ (e) => setEmail(e.target.value) }
                             className="w-full px-4 py-2 border border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus;ring-blue-400"
                             />
-                            <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+                            <p className="text-red-500 text-sm mt-1">{loginError}</p>
                         </div>
 
                         <div className="mb-4">
@@ -59,15 +74,14 @@ function LoginSignUpPage() {
                             onChange={ (e) => setPassword(e.target.value) }
                             className="w-full px-4 py-2 border border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus;ring-blue-400"
                             />
-                            <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+                            <p className="text-red-500 text-sm mt-1">{loginError}</p>
                         </div>
 
                             <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg transition duration-200">Login</button>
-                            {success && <p className="text-green-600 text-center mt-4 font-medium">{success}</p>}
                     </form>
                     <div className="items-center justify-center">
                         <button onClick={handleSignUp} className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg transition duration-200">Sign Up</button><br />
-                        <button onClick={() => signInWithPopup(auth, new GoogleAuthProvider())} className="w-full flex items-center justify-center gap-2 border border-gray-300 hover:bg-gray-50 py-2 rounded-lg transition">
+                        <button onClick={ handleLoginWithGoogle} className="w-full flex items-center justify-center gap-2 border border-gray-300 hover:bg-gray-50 py-2 rounded-lg transition">
                             <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-5 h-5" alt="Google" />
                             Sign in with Google
                         </button>
@@ -81,4 +95,4 @@ function LoginSignUpPage() {
     );
 }
 
-export default LoginSignUpPage;
+export default LoginSignUpPage
