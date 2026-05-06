c7af48d8389c2a672460c614412f218154978dbc
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-06 16:58:01 +0300

    Voting fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Routes, Route} from "react-router-dom"
 import VotingPage from "./Pages/Voting-page"  
 import AboutPage from "./Pages/About-Page"
 import LoginSignUpPage from "./Pages/Login-SignUp-page"
+import { HashRouter as Router } from "react-router-dom"
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/voting" element={<VotingPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/" element={<LoginSignUpPage />} />
-    </Routes>
+    <Router>
+        <Routes>
+          <Route path="/voting" element={<VotingPage />} />
+          <Route path="/" element={<LoginSignUpPage />} />
+        </Routes>
+    </Router>
   );
 }
 
